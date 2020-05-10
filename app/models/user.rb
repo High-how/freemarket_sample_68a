720cccfdde58597e8d2b2f5783ff8df5f78e1887
High-how <1215ikkou@gmail.com>
2020-05-10 23:27:31 +0900

    .rspec-test
    単体テストのクリア

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :family_name, :first_name, :family_name_reading, :first_name_reading, :birth_year, :birth_month, :birth_day, presence: true
+  validates :password,length: { in: 7..128} 
   has_one :address
 end
