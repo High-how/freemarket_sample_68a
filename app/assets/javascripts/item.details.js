69ed82919b0e3b0176a7ef80747ddffb553b9e62
fy-0621 <60965937+fy-0621@users.noreply.github.com>
2020-05-24 15:32:13 +0900

    js修正

--- app/assets/javascripts/item.details.js
+++ app/assets/javascripts/item.details.js
@@ -1,5 +1,5 @@
 $(function(){
-$('.exhibition-item__main-content__item-photos__other-photos img').click(function(){
+$('.exhibition-item__main-content__item-photos__other-photos img').mouseover(function(){
 var $thisImg = $(this).attr('src');
 var $thisAlt = $(this).attr('alt');
 $('.exhibition-item__main-content__item-photos__choice-photo img').attr({src:$thisImg,alt:$thisAlt});
