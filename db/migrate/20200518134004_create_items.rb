d0459b9cabd79fed2fd24fe7ff2f1f1b307097bf
nanami96 <flt.nnm.sbt@gmail.com>
2020-05-25 23:59:37 +0900

    sell-page-delete-file

--- db/migrate/20200518134004_create_items.rb
+++ db/migrate/20200518134004_create_items.rb
@@ -1,21 +1,20 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string     :name,             null: false      
-      t.string     :postage_type,     null: false      
-      t.string     :shipping_area,    null: false      
-      t.string     :brand      
-      t.string     :price,            null: false, foreign_key: true      
-      t.integer    :trading_status,   null: false, limit: 1      
-      t.integer    :shipping_date,    null: false, limit: 1      
-      t.integer    :postage_payer,    null: false, limit: 1  
-      t.integer    :size,             null: false, limit: 1     
-      t.text       :introduction,     null: false      
-      t.references :user,             null: false, foreign_key: true      
-      t.references :category,         null: false, foreign_key: true      
-      # t.references :buyer_id,         null: false, foreign_key: true      
-      # t.references :seller_id,        null: false, foreign_key: true      
-
+      t.string     :name,             null: false
+      t.string     :postage_type,     null: false
+      t.string     :shipping_area,    null: false
+      t.string     :brand
+      t.string     :trading_status,    null: false
+      t.string     :shipping_date,     null: false
+      t.integer    :size,             null: false
+      t.string     :postage_payer,     null: false
+      t.integer    :price,            null: false, foreign_key: true
+      t.integer    :seller_id
+      t.integer    :buyer_id
+      t.text       :introduction,     null: false
+      t.references :user,             null: false, foreign_key: true
+      t.references :category,         null: false, foreign_key: true
       t.timestamps
     end
   end
