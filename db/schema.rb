7e20956fc6a496849573bc6c759f7eec8bda4cc7
High-how <1215ikkou@gmail.com>
2020-05-27 21:43:15 +0900

    devise-error

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2020_05_17_054704) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["phone_number"], name: "index_addresses_on_phone_number", unique: true
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
