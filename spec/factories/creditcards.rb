b1bbc028a9fd68086006b6788bbe41932225a50a
fy-0621 <60965937+fy-0621@users.noreply.github.com>
2020-06-19 21:42:22 +0900

    kkk

--- spec/factories/creditcards.rb
+++ spec/factories/creditcards.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
 
   factory :creditcard do
-    #id                 {"1"}
-    #user_id            {"1"}
-    #number             {"424242424242424242"}
-    #exp_month          {"15"}
-    #exp_year           {"29"}
-    #cvc                {"123"}
+    # id                 {"1"}
+    # user_id            {"1"}
+    # number             {"424242424242424242"}
+    # exp_month          {"15"}
+    # exp_year           {"29"}
+    # cvc                {"123"}
     card_id               {"car_aaaaaaa"}
     payjp_id              {"cus_aaaaaaa"}
     user
-    #created_at         {"2020-06-08 16:15:28"}
-    #updated_at         {"2020-06-08 16:15:28"}
+    # created_at         {"2020-06-08 16:15:28"}
+    # updated_at         {"2020-06-08 16:15:28"}
   end
 
 end
