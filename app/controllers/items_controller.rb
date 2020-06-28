7520e204166ac0b4a5c868ba45884e47caa4fa0a
nanami96 <flt.nnm.sbt@gmail.com>
2020-06-28 15:26:19 +0900

    index-main-update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create, :category_search]
 
   def index
-    @items = Item.all
+    @items = Item.all.order("id DESC")
   end
 
   def new
